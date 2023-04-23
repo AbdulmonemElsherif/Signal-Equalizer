47f0cc79f94dddfd73da90c606d09d1bff7bc768
asmaahamza1 <asmaahaamza@gmail.com>
2023-04-23 02:11:34 +0200

    csv upload doesn't work

--- app.py
+++ app.py
@@ -5,5 +5,85 @@ import librosa
 import json
 import soundfile as sf
 import io
+import csv
+import wave
+import struct
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#to upload the file 
+@app.route('/uploadfile', methods=['POST'])
+def uploadfile():
+    file = request.files['file']
+    if file and file.filename.endswith('.csv'):
+        csv_data = []
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            csv_data.append(row)
+        return jsonify(csv_data)
+
+#to detect arrythmia
+# @app.route('/detectArrythmia', methods=['POST'])
+# def detectArrythmia():
+#     file = request.files['file']
+#     if file.filename.endswith('.csv'):
+#         arrhythmia_values = []
+#         csv_reader = csv.DictReader(file)
+#         for row in csv_reader:
+#             # Replace 'column_name' with the name of the column containing arrhythmia values
+#             arrhythmia_values.append(float(row['column_name']))
+#         return jsonify(arrhythmia_values)
+#     else:
+#         return jsonify({'error': 'Invalid file type'})
+
+    
+
+# def csv_to_wav(csv_filename, wav_filename):
+#     # Open the CSV file and read in the time and audio data
+#     with open(csv_filename, 'r') as f:
+#         reader = csv.reader(f)
+#         time_data = []
+#         audio_data = []
+#         for row in reader:
+#             time_data.append(float(row[0]))
+#             audio_data.append(float(row[1]))
+
+#     # Normalize the audio data
+#     max_value = max(audio_data)
+#     min_value = min(audio_data)
+#     audio_data = [(x - min_value) / (max_value - min_value) for x in audio_data]
+
+#     # Create a WAV file and write the audio data to it
+#     with wave.open(wav_filename, 'w') as wavfile:
+#         wavfile.setnchannels(1)
+#         wavfile.setsampwidth(2)
+#         wavfile.setframerate(44100)
+#         for i in range(len(time_data)):
+#             sample = int(audio_data[i] * 32767)
+#             data = struct.pack('<h', sample)
+#             wavfile.writeframes(data)
+            
+#          return send_file(wav_filename, mimetype='audio/wav')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -6,56 +6,117 @@ const inputSpectrogram = document.getElementById("inputspectrogram");
 const outputSpectrogram = document.getElementById("outputspectrogram");
 // Define variables for equalizer sliders
 let sliders;
+let sampleRate;
 // Define variable for mode selector
 const equalizerContainer = document.getElementById("equalizer-container");
-const modeSelector = document.getElementById("mode-select");
+const modeSelector = document.getElementById("mode-select");  
 const inputAudio = document.getElementById("inputaudio");
+const outputAudio = document.getElementById("outputaudio");
+
+let file;
 
 
-// Define function to update equalizer sliders based on selected mode
-function updateSliders() {
-  // Remove existing sliders
-  if (sliders) {
-    sliders.remove();
-  }
-  // Create new sliders based on selected mode
-  const mode = modeSelector.value;  
-  // Hide the sliders if no mode is selected
-  if (mode == "select-mode") {
-    equalizerContainer.style.display = "none";
-  } else {
-    equalizerContainer.style.display = "block";
-  }
-  switch (mode) {
-    case "uniform-range":
-      sliders = document.getElementById("uniform-range-sliders").cloneNode(true);
-      break;
-    case "vowels":
-      sliders = document.getElementById("vowels-sliders").cloneNode(true);
-      break;
-    case "musical-instruments":
-      sliders = document.getElementById("musical-instruments-sliders").cloneNode(true);
-      break;
-    case "biological-signal-abnormalities":
-      sliders = document.getElementById("biological-signal-abnormalities-sliders").cloneNode(true);
-      break;
-    default:
-      sliders = document.createElement("div");
-      break;
-  }
-  // Add new sliders to equalizer container
-  equalizerContainer.appendChild(sliders);
-}
+document.querySelectorAll(".slider").forEach((slider)=>{
+  slider.addEventListener("change", handleSliderChange)
+});
 
 // Add event listener to mode selector
-modeSelector.addEventListener("change", updateSliders);
+modeSelector.addEventListener("change", ()=>{
+  const selectedIndex = modeSelector.selectedIndex;
+  updateSliders(selectedIndex);
+});
 
 window.addEventListener("load", function () {
   createPlot(inputSignal);
   createPlot(outputSignal);
-  // Call updateSliders function to initialize equalizer sliders
-  updateSliders();
 });
+
+// document
+//   .getElementById("formFile")
+//   .addEventListener("change", async (event) => {
+//     file = event.target.files[0];
+//     let fileURL = URL.createObjectURL(file);
+//     inputAudio.src = fileURL;
+//     outputAudio.src = fileURL;
+//     var formData = new FormData();
+//     formData.append("audioFile", file);
+//     fetch("/readAudioFile", {
+//       method: "POST",
+//       body: formData,
+//     })
+//       .then((response) => response.json())
+//       .then((result) => {
+//         let audioDataArray = result.audioData;
+//         let time = [];
+//         sampleRate = result.sampleRate;
+//         for (let index = 0; index < audioDataArray.length; index++) {
+//           //get time from sampling frequency as  fs = 1/T
+//           time.push(index / sampleRate);
+//         }
+//         document.querySelectorAll(".slider").forEach((slider) => {
+//           slider.value = 0;
+//         });
+//         if (inputSignal.data.length === 0) {
+//           Plotly.addTraces(inputSignal, { x: time, y: audioDataArray });
+//           Plotly.addTraces(outputSignal, { x: time, y: audioDataArray });
+//         } else {
+//           Plotly.deleteTraces(inputSignal, [0]);
+//           Plotly.deleteTraces(outputSignal, [0]);
+//           Plotly.addTraces(inputSignal, { x: time, y: audioDataArray });
+//           Plotly.addTraces(outputSignal, { x: time, y: audioDataArray });
+//         }
+//       });
+//     // // Add a vertical line trace for the cursor
+//     // // const max = Math.max(audioData);
+//     // Plotly.addTraces(inputSignal, { x: [0, 0], y: [-0.5, 0.5] });
+//     // Plotly.addTraces(outputSignal, { x: [0, 0], y: [-0.5, 0.5] });
+//   });
+
+  document.getElementById("formfile").addEventListener("change", async (event)=> {
+    const file = document.getElementById("formfile").files[0];
+    const fileType = file.type;
+    let data;
+    if(fileType == 'text/csv')
+    {
+      const formData = new FormData();
+      formData.append('file', file);
+      fetch('/upload_csv', {
+          method: 'POST',
+          body: formData
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        if (inputSignal.data.length === 0) {
+          const reader = new FileReader();
+          reader.readAsText(file);
+          reader.onload = () => {
+          data = reader.result
+          .trim()
+          .split("\n")
+          .map((row) => {
+          const [col1, col2] = row.split(",");
+          return { col1: parseFloat(col1), col2: parseFloat(col2) };
+          });
+        }
+      }
+      convertCsvToTrace(data);
+
+    });
+  }
+ });
+
+ function convertCsvToTrace(csvdata) {
+  // Extract data from the CSV data
+  let x = csvdata.map((arrRow) => arrRow.col1).slice(0, 1000);
+  let y = csvdata.map((arrRow) => arrRow.col2).slice(0, 1000);
+  let uploadedSignal = { name: "Original Signal", x: x, y: y };
+  // If there are no existing signals, add the uploaded signal as a trace to the plot else add the uploaded signal as a component to the plot
+  if (signals.length == 0) {
+    signals.push(uploadedSignal);
+    Plotly.addTraces(signalGraph, uploadedSignal);
+  }
+}
+
 function createPlot(graphElement) {
   let layout = {
     xaxis: {
@@ -75,47 +136,83 @@ function createPlot(graphElement) {
   });
 }
 
-document.getElementById("formFile").addEventListener("change", async (event) => {
-  let file = event.target.files[0];
-  let fileURL = URL.createObjectURL(file);
-  inputAudio.src = fileURL;
-  const audioContext = new AudioContext();
-  // Read the audio file as an ArrayBuffer
-  const arrayBuffer = await file.arrayBuffer();
-  // Decode the audio file data directly using await
-  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-  // Extract the audio data from the audio buffer (amplitude)
-  const audioData = audioBuffer.getChannelData(0);
-  console.log(audioData);
-  let time=[]
-  for (let index = 0; index < audioData.length; index++) {
-    //get time from sampling frequency as  fs = 1/T
-    time.push(index / audioBuffer.sampleRate);
-  }
-  Plotly.addTraces(inputSignal, { x: time, y: audioData }, 0);
-  Plotly.addTraces(outputSignal, { x: time, y: audioData }, 0);
-  track.connect(audioContext.destination);
-});
+// Define function to update equalizer sliders based on selected mode
+function updateSliders(selectedIndex) {
+  const sliderGroups = document.querySelectorAll(".slider-group");
+  equalizerContainer.style.display = "block";
+  sliderGroups.forEach((sliderGroup, index) => {
+    if (index === selectedIndex - 1) {
+      sliderGroup.style.display = "block";
+    } else {
+      sliderGroup.style.display = "none";
+    }
+  });
+}
 
-document.querySelectorAll(".stopbutton").forEach((button,index) => {
+function getSliderValues(){
+  let sliderValues = [];
+  selectedModeIndex = document.getElementById("mode-select").selectedIndex;
+  if (selectedModeIndex === 1) {
+    document.querySelectorAll(".uniformmode").forEach((slider) => {
+      sliderValues.push(slider.value);
+    });
+  } else if (selectedModeIndex === 2) {
+    document.querySelectorAll(".vowels").forEach((slider) => {
+      sliderValues.push(slider.value);
+    });
+  }
+  return sliderValues;
+}
+
+function handleSliderChange() {
+  sliderValues = getSliderValues();
+  var formData = new FormData();
+  formData.append("audioFile", file);
+  formData.append("sliderValues", JSON.stringify(sliderValues));
+  fetch("audioProcessing", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.blob())
+    .then((result) => {
+      console.log(result);
+      outputAudio.src = URL.createObjectURL(result);
+      processUniformAudio(result);
+    });
+  }
+
+  
+document.querySelectorAll(".stopbutton").forEach((button, index) => {
   button.addEventListener("click", () => {
-    if(index===0){
+    if (index === 0) {
       inputAudio.pause();
       inputAudio.currentTime = 0;
-    }else{
+    } else {
       outputAudio.pause();
       outputAudio.currentTime = 0;
     }
   });
 });
 
-document.querySelector("#spectrogram-toggle").addEventListener('change',(event)=>{
-  document.querySelectorAll(".spectrogram").forEach((spectrogram)=>{
-      if (event.target.checked) {
-          spectrogram.style.display='block';
-        }
-        else{
-          spectrogram.style.display = "none";
-        }
-      })
+document.querySelector("#spectrogram-toggle").addEventListener("change", (event) => {
+  document.querySelectorAll(".spectrogram").forEach((spectrogram) => {
+    if (event.target.checked) {
+      spectrogram.style.display = "block";
+    } else {
+      spectrogram.style.display = "none";
+    }
+  });
 });
+
+document.getElementById("ECG").addEventListener("change", function(){
+  const file = document.getElementById('ECG').files[0];
+
+  const formData = new FormData();
+  formData.append('r', file);
+
+  axios.post('', formData)
+    .then(response => {
+      
+      const graphData = JSON.parse(response.data);
+    });
+});                                                                     
